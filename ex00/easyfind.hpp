10bd729a06e8eee57fe69b2f5d7b42089da9bccf
jeongyunnim <jeongyun7521@gmail.com>
2023-07-31 06:06:37 +0900

    feat: ex00 test문 러프하게 작성 완료
    100개 테스트하고, 찾은 문자열의 반복자를 역참조하여 출력 값을
    보여주었다.

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -14,26 +14,13 @@ class ValueNotFoundException : public std::exception
 	}
 };
 
-class InvalidContainerTypeException : public std::exception
-{
-	const char *what(void) const throw()
-	{
-		return ("Invalid container type.");
-	}
-};
-
 template <typename T>
 typename T::iterator easyfind(T& container, int value)
 {
-	T::iterator it = container.begin();
-	if (*it != static_cast<int>(*it) || *it != reinterpret_cast<int>(*it))
-		throw (InvalidContainerTypeException());
-	for (it; it != container.end(); it++)
-	{
-		if (*it == value)
-			return (it);
-	}
-	throw (ValueNotFoundException());
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw (ValueNotFoundException());
+	return (it);
 }
 
 #endif
---
+++ ex00/main.cpp
@@ -0,0 +1,29 @@
+#include "easyfind.hpp"
+#include <vector>
+
+int main()
+{
+	static const int vectorSize = 100;
+	std::vector<int> intVector(vectorSize);
+	int i = 0;
+	for (std::vector<int>::iterator it = intVector.begin(); it != intVector.end(); it++)
+	{
+		*it = i++;
+	}
+
+	try
+	{
+		for (int i = 0;; i++)
+		{
+			std::vector<int>::iterator pos = easyfind(intVector, i);
+			std::cout << *pos << std::endl;
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+
+	return (0);
+}
